6b3aff7643254893ca4c36856df7598a607332ee
Omar Issa <omom2168@gmail.com>
2025-10-23 02:10:40 +0300

    fix minimum allowed length error msg

--- controllers/main.js
+++ controllers/main.js
@@ -2,21 +2,34 @@ import User from "../models/user.js";
 import BadRequestError from "../errors/bad-request.js";
 import UnauthenticatedError from "../errors/unauthenticated.js";
 
-const register = async (req, res) => {
-  const { username, password } = req.body;
+const register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
 
-  if (!username || !password) {
-    throw new BadRequestError("Please provide username and password");
+    if (!username || !password) {
+      throw new BadRequestError("Please provide username and password");
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      throw new BadRequestError("Username already exists");
+    }
+
+    const user = await User.create({ username, password });
+    const token = user.createJWT();
+
+    res.status(201).json({ user: { name: user.username }, token });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const message = Object.values(error.errors)
+        .map((val) => val.message)
+        .join(", ");
+      return res.status(400).json({ msg: message });
+    }
+
+    next(error);
   }
-  const existingUser = await User.findOne({ username });
-  if (existingUser) {
-    throw new BadRequestError("Username already exists");
-  }
-  const user = await User.create({ username, password });
-  const token = user.createJWT();
-  res.status(201).json({ user: { name: user.username }, token });
 };
-
 const login = async (req, res) => {
   const { username, password } = req.body;
 
